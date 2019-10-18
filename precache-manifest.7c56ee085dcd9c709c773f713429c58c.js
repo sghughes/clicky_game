9a1244e736422b412387f81e9264c64181e7776a
Stephanie Hughes <stephaniegracehughes@gmail.com>
2019-10-17 18:34:41 -0700

    Updates

---
+++ precache-manifest.7c56ee085dcd9c709c773f713429c58c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462c8e0ff12740422b5f33414450202e",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "bc6d4a5e7707fd759b6d",
+    "url": "/clicky_game/static/css/main.e18528a9.chunk.css"
+  },
+  {
+    "revision": "7bcda612e7e3167e85cf",
+    "url": "/clicky_game/static/js/2.fc710326.chunk.js"
+  },
+  {
+    "revision": "bc6d4a5e7707fd759b6d",
+    "url": "/clicky_game/static/js/main.3102d909.chunk.js"
+  },
+  {
+    "revision": "787559cc3978be4177b6",
+    "url": "/clicky_game/static/js/runtime-main.d0664b11.js"
+  }
+]);
